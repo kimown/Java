3d0af1aae9afb8fedaff756a3bb6468729e76c26
kimow <kimown@163.com>
2015-03-29 22:45:32 +0800

    http://docs.oracle.com/javase/tutorial/jdbc/index.html
    20150329 22：45 翻译结束

---
+++ src/com/oracle/tutorial/jdbc/translation.java
@@ -0,0 +1,36 @@
+package com.oracle.tutorial.jdbc;
+
+public class translation {
+	
+	/**
+	 * http://docs.oracle.com/javase/tutorial/jdbc/index.html
+	 */
+	public void a(){
+/*  
+JDBC™ API被设计用于让简单的事情更简单。这意味着JDBC让日常的数据库任务简单。这份
+教程将会通过简单的使用JDBC执行常用SQL语句、完成其他对于数据库应用很常见的其他目的。
+这份教程分为以下课程“
+JDBC介绍：列举JDBC特征，描述JDBC架构和预览SQL命令和关系数据苦的概念。
+JDBC基础：涉及JDBC API，这些包含在 Java™标准版第六版的发布文件里面。
+通过本次课程，你将会知道怎样使用基础的JDBC API来建表，插入数据，查询数据，
+从查询的数据集中检索结果和更新数据。在这个过程中，你将会学会怎么样使用简单的
+声明和预编译声明，你将会看到一个使用存储过程的例子。你也会学会怎么样执行事务
+和捕获异常和警告。
+
+	
+
+*
+*/
+
+
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
